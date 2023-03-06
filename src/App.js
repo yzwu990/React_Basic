3978d71135ab99cd1787b577700fcb5a1d5eb1a0
Yz Wu <yzwu990@gmail.com>
2023-03-06 00:42:03 -0500

    setState callback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: { firstName: "John", lastName: "Doe" },
+      company: "Facebook"
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name.firstName} {this.state.lastName}, I work
+            for {this.state.company}
+          </p>
+          <button onClick={() => {
+            this.setState(
+              // could be (state,props) => {
+              // state is the current state, in case we need to render the state according to the current state
+              // props is the current props
+              // for now we are not using props and state.
+              () => {
+                return {
+                  name: { firstName: "Jane", lastName: "May" }
+                };
+              },
+              () => {
+                console.log(this.state);
+              }
+            );
+          }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
